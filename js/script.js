32d5acbff8cc9e3edc64a3500c1c3db245f554a7
Emanuele <emanuele65b@gmail.com>
2021-05-27 15:53:08 +0200

    finishing project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 
-// KM
+// km
 var km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 
-// ETA'
-var eta = parseInt(prompt('Quanti anni hai?'));
+// età
+var età = parseInt(prompt('Quanti anni hai?'));
 
+var prezzo_biglietto = 0.21 * km;
+
+// operazione prezzo
+var minorenne = prezzo_biglietto - prezzo_biglietto * 20 / 100;
+
+// risultato prezzo
+document.getElementById(minorenne);
