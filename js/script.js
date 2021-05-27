30ef646b30b21213fe72e5c14c28a275d3f3708d
Emanuele <emanuele65b@gmail.com>
2021-05-27 16:59:02 +0200

    add sconto età

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+// Questo richiederà un minimo di ricerca.
+
+
 // km
 var km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 
 // età
 var età = parseInt(prompt('Quanti anni hai?'));
 
+// operazione prezzo
 var biglietto = 0.21 * km;
 
-// operazione prezzo
-var minorenne = biglietto - (biglietto * 20) / 100;
+
+// if else
+if(età < 18){
+    var minorenne = biglietto - (biglietto * 20) / 100;
+} else (età > 65){
+    var over60 = biglietto - (biglietto * 20) / 100;
+}
+
+var scatola = biglietto;
 
 // risultato prezzo
-document.getElementById(minorenne);
+document.getElementById('pippo').innerHTML = scatola;
