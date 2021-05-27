132118c29cb7cca93e5deea9a2ce2abae0c35da0
Emanuele <emanuele65b@gmail.com>
2021-05-27 16:08:25 +0200

    add biglietto

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ var km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 // età
 var età = parseInt(prompt('Quanti anni hai?'));
 
-var prezzo_biglietto = 0.21 * km;
+var biglietto = 0.21 * km;
 
 // operazione prezzo
-var minorenne = prezzo_biglietto - prezzo_biglietto * 20 / 100;
+var minorenne = biglietto - (biglietto * 20) / 100;
 
 // risultato prezzo
 document.getElementById(minorenne);
