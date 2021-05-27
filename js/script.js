2eccb7dd96d2d85112cdc6dae1e8510d7c7ca811
Emanuele <emanuele65b@gmail.com>
2021-05-27 17:15:36 +0200

    add miglioramenti e correzioni

--- js/script.js
+++ js/script.js
@@ -14,14 +14,14 @@ var età = parseInt(prompt('Quanti anni hai?'));
 // operazione prezzo
 var biglietto = 0.21 * km;
 
+var scatola = biglietto;
+
 // if else
 if(età < 18){
     scatola = biglietto - (biglietto * 20) / 100;
-} else (età > 65){
+} else if(età > 65){
     scatola = biglietto - (biglietto * 40) / 100;
 }
-
-var scatola = biglietto;
 
 // risultato prezzo
 document.getElementById('biglietto').innerHTML = scatola;
